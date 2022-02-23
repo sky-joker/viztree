868ee50c376100b0c6b38f2ac960564181517cd0
sky-joker <sky.jokerxx@gmail.com>
2022-02-23 21:56:57 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup
+import pathlib
 
-setup()
+here = pathlib.Path(__file__).parent.resolve()
+install_requires = (here / 'requirements.txt').read_text(encoding='utf-8').splitlines()
+
+setup(
+    install_requires=install_requires
+)
